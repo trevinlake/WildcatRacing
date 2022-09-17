70c21871e084ef244761f8a2f04cb77b08db832b
trevinlake <trevinlake@mail.weber.edu>
2022-09-17 09:15:51 -0600

    Pulse widths and calibration complete

--- catkin_ws/src/wildcat_racing/scripts/servo_and_drive_calibration.py
+++ catkin_ws/src/wildcat_racing/scripts/servo_and_drive_calibration.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from adafruit_servokit import ServoKit
 import numpy as np
 import time
 
 kit = ServoKit(channels=16)
+
+kit.servo[0].set_pulse_width_range(1400, 1825)
 
 """
 #Use this code to set pulse width min and max.
@@ -30,11 +35,12 @@ STEERING_MIN_ANGLE = 0
 STEERING_MAX_ANGLE = 180
 STEERING_STEP = 0.2
 STEERING_STEP_LIST = np.arange(STEERING_MIN_ANGLE, STEERING_MAX_ANGLE, STEERING_STEP)
+INVERSE_STEERING_STEP_LIST = np.arange(STEERING_MAX_ANGLE, STEERING_MIN_ANGLE, STEERING_STEP)
 
 #Drive Constants
 DRIVE_THROTTLE_MIN = 0
-DRIVE_THROTTLE_MAX = 0.25
-DRIVE_STEP = 0.01
+DRIVE_THROTTLE_MAX = 0.05
+DRIVE_STEP = 0.001
 DRIVE_STEP_LIST = np.arange(DRIVE_THROTTLE_MIN, DRIVE_THROTTLE_MAX, DRIVE_STEP)
 
 
@@ -42,9 +48,18 @@ DRIVE_STEP_LIST = np.arange(DRIVE_THROTTLE_MIN, DRIVE_THROTTLE_MAX, DRIVE_STEP)
 for step in STEERING_STEP_LIST:
     kit.servo[0].angle = step
     print(step)
-    time.sleep(.1)
+    time.sleep(.01)
+
+time.sleep(5)
 
 #Slowly increase throttle on drive motor within its entire range.
 for step in DRIVE_STEP_LIST:
     kit.continuous_servo[2].throttle = step
+    time.sleep(.1)
     print(step)
+
+
+
+kit.continuous_servo[2].throttle = 0
+
+kit.servo[0].angle = 90
