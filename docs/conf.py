7990a93229ebb9ec147d22d697e39d90525dfe75
trevinlake <84420356+trevinlake@users.noreply.github.com>
2022-11-30 11:49:05 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
 
 project = 'WildcatRacing'
 copyright = '2022, Trevin Lake, Ryan Reidhead'
@@ -24,5 +25,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
+
